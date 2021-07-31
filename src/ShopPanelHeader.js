918d4c4a95f3a114941f034fb039f4f8a3d8f412
tomcel <tcel@protonmail.com>
2021-07-31 15:35:16 +0200

    shop panel refactor

--- src/ShopPanelHeader.js
+++ src/ShopPanelHeader.js
@@ -1,43 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { getUser } from './api';
 import { Link, Route} from "react-router-dom";
 
-export default class ShopPanelHeader extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        user: undefined,
-        isLoaded: false
-      }
-    }
+const ShopPanelHeader = () => {
+  const [user, setUser] = useState();
   
-    async componentDidMount() {
-      const user = await getUser();
-      this.setState({ 
-        user: user,
-        isLoaded: true
-      });
+  useEffect(async () => {
+    const user = await getUser();
+    if(user) {
+      setUser(user);
     }
-  
-    render() {
-      const { user, isLoaded } = this.state;
+  })
 
-      if(isLoaded) {
-        return (
-        <div className="container py-2">
-          <div className="d-flex">
-            <div className="d-flex flex-row" style={{width: "80vw"}}>
-              <h1 className="text-light"><b>Web Shop Panel</b></h1>
-            </div>
-              <button className="btn btn-danger float-right btn-sm" style={{height: "50%"}}>Log Off</button>
+  if(user) {
+    return (
+      <div className="container py-2">
+        <div className="d-flex">
+          <div className="d-flex flex-row" style={{width: "80vw"}}>
+            <h1 className="text-light"><b>Web Shop Panel</b></h1>
           </div>
-          <div className="pb-2 text-light">Logged as: <b>{user.name}</b></div>
+            <button className="btn btn-danger float-right btn-sm" style={{height: "50%"}}>Log Off</button>
         </div>
-        );
-      }
-      else {
-        return "Loading...";
-      }
-    }
+        <div className="pb-2 text-light">Logged as: <b>{user.name}</b></div>
+      </div>
+    );
   }
+  else {
+    return "Loading...";
+  }
+}
+
+export default ShopPanelHeader;
+
+
+// export default class ShopPanelHeader extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         user: undefined,
+//         isLoaded: false
+//       }
+//     }
+  
+//     async componentDidMount() {
+//       const user = await getUser();
+//       this.setState({ 
+//         user: user,
+//         isLoaded: true
+//       });
+//     }
+  
+//     render() {
+//       const { user, isLoaded } = this.state;
+
+//       if(isLoaded) {
+//         return (
+//         <div className="container py-2">
+//           <div className="d-flex">
+//             <div className="d-flex flex-row" style={{width: "80vw"}}>
+//               <h1 className="text-light"><b>Web Shop Panel</b></h1>
+//             </div>
+//               <button className="btn btn-danger float-right btn-sm" style={{height: "50%"}}>Log Off</button>
+//           </div>
+//           <div className="pb-2 text-light">Logged as: <b>{user.name}</b></div>
+//         </div>
+//         );
+//       }
+//       else {
+//         return "Loading...";
+//       }
+//     }
+//   }
