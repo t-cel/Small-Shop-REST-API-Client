de90b20d9050cbe99f523119c3b5cf14beef7257
tomcel <tcel@protonmail.com>
2021-08-04 21:28:13 +0200

    api cleanup

--- src/api.js
+++ src/api.js
@@ -2,19 +2,41 @@ if(!localStorage.getItem("userCredidentials")) {
   localStorage.setItem("userCredidentials", JSON.stringify({ email: "", password: "" }));
 }
 
+const baseUrl = 'https://localhost:44302/shop_api';
+
+// requests headers
 const getAuthHeader = () => {
   const userCredidentials = JSON.parse(localStorage.getItem("userCredidentials"));
   return `Basic ${Buffer.from(`${userCredidentials.email}:${userCredidentials.password}`).toString('base64')}`;
 }
-  
-const baseUrl = 'https://localhost:44302/shop_api';
 
-const createHeaders = () => { return { 
+const createBaseHeaders = () => ({
   'Authorization': getAuthHeader(),
   'Access-Control-Allow-Origin': '*',
-  'Accept': 'application/json'
-}}
+});
 
+const createGetHeaders = () => {
+  const base = createBaseHeaders();
+  base['Accept'] = 'application/json';
+  return base;
+};
+
+const createPostHeaders = () => {
+  const base = createBaseHeaders();
+  base['Content-Type'] = 'application/json';
+  base['Accept'] = 'application/json';
+  return base;
+};
+
+const createPutHeaders = () => {
+  const base = createBaseHeaders();
+  base['Content-Type'] = 'application/json';
+  return base;
+}
+
+const createDeleteHeaders = () => createBaseHeaders();
+
+// general error returned when client cannot reach API
 export const APIRespondError = "APIFailure";
 
 const fetchedCorrectly = (res) => res.status >= 200 && res.status < 300;
@@ -28,7 +50,7 @@ async function fetchDataFromAPI(url, init) {
         return null;
       }
     } else {
-      return new Error(`Error on fetching data from API, error code: ${res.status}`);  
+      throw new Error(`Error on fetching data from API, error code: ${res.status}`);  
     }
   }).catch(e => APIRespondError);
 }
@@ -37,7 +59,7 @@ async function fetchDataFromAPI(url, init) {
 
 export async function getCategories() {
   const url = `${baseUrl}/categories`;
-  const init = { method: 'GET', headers: createHeaders()};
+  const init = { method: 'GET', headers: createGetHeaders()};
   return await fetchDataFromAPI(url, init);
 }
 
@@ -45,62 +67,44 @@ export async function getCategories() {
 
 export async function getProducts(query) {
   const url = `${baseUrl}/products${query}`;
-  const init = { method: 'GET', headers: createHeaders()};
+  const init = { method: 'GET', headers: createGetHeaders()};
   return await fetchDataFromAPI(url, init);
 }
 
 export async function getProduct(id) {
   const url = `${baseUrl}/products/${id}`;
-  const init = { method: 'GET', headers: createHeaders()};
+  const init = { method: 'GET', headers: createGetHeaders()};
   return await fetchDataFromAPI(url, init);
 }
 
 export async function createProduct(product) {
   const url = `${baseUrl}/products/`;
-  const postHeaders = {
-    'Authorization': getAuthHeader(),
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
-  const init = { method: 'POST', headers: postHeaders, body: JSON.stringify(product)};
+  const init = { method: 'POST', headers: createPostHeaders(), body: JSON.stringify(product)};
   return await fetchDataFromAPI(url, init);
 }
 
 export async function updateProduct(id, product) {
   const url = `${baseUrl}/products/${id}`;
-  const putHeaders = {
-    'Authorization': getAuthHeader(),
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
-  }
-  const init = { method: 'PUT', headers: putHeaders, body: JSON.stringify(product)};
+  const init = { method: 'PUT', headers: createPutHeaders(), body: JSON.stringify(product)};
   return await fetchDataFromAPI(url, init);
 }
 
 export async function deleteProduct(id) {
   const url = `${baseUrl}/products/${id}`;
-  const putHeaders = {
-    'Authorization': getAuthHeader(),
-    'Access-Control-Allow-Origin': '*',
-  }
-  const init = { method: 'DELETE', headers: putHeaders};
+  const init = { method: 'DELETE', headers: createDeleteHeaders()};
   return await fetchDataFromAPI(url, init);
 }
 
 export async function getProductImages(productId) {
   const url = `${baseUrl}/products/${productId}/images`;
-  const init = { method: 'GET', headers: createHeaders() };
+  const init = { method: 'GET', headers: createGetHeaders() };
   return await fetchDataFromAPI(url, init);
 }
 
 export async function addProductImage(productId, productImage) {
   const url = `${baseUrl}/products/${productId}/images`;
-  const postHeaders = {
-    'Authorization': getAuthHeader(),
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-  }
+  const postHeaders = createPostHeaders();
+  postHeaders['Accept'] = '*/*'; //API doesn't return any JSON when adding image to product
   const init = { method: 'POST', headers: postHeaders, body: JSON.stringify(productImage) };
   return await fetchDataFromAPI(url, init);
 }
@@ -109,19 +113,13 @@ export async function addProductImage(productId, productImage) {
 
 export async function uploadImage(image) {
   const url = `${baseUrl}/images/`;
-  const postHeaders = {
-    'Authorization': getAuthHeader(),
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
-  const init = { method: 'POST', headers: postHeaders, body: JSON.stringify(image) };
+  const init = { method: 'POST', headers: createPostHeaders(), body: JSON.stringify(image) };
   return await fetchDataFromAPI(url, init);
 }
 
 export async function getImage(imageURL) {
   const url = `${baseUrl}/images/${imageURL}`;
-  const init = { method: 'GET', headers: createHeaders() };
+  const init = { method: 'GET', headers: createGetHeaders() };
 
   return await fetch(url, init).then((res) => {
     if(fetchedCorrectly(res)) {
@@ -137,17 +135,12 @@ export async function getImage(imageURL) {
 
 export async function getUser() {
   const url = `${baseUrl}/user`;
-  const init = { method: 'GET', headers: createHeaders() };
+  const init = { method: 'GET', headers: createGetHeaders() };
   return await fetchDataFromAPI(url, init);
 }
 
 export function setCredidentials(login, password) {
-  const userCredidentials = {
-    email: login, 
-    password: password
-  }
-
-  localStorage.setItem("userCredidentials", JSON.stringify(userCredidentials));
+  localStorage.setItem("userCredidentials", JSON.stringify({email: login, password: password}));
 }
 
 export function logOff() {
@@ -163,39 +156,24 @@ export function wasLoggedIn() {
 
 export async function getOrders(query) {
   const url = `${baseUrl}/orders${query}`;
-  const init = { method: 'GET', headers: createHeaders() };
+  const init = { method: 'GET', headers: createGetHeaders() };
   return await fetchDataFromAPI(url, init); 
 }
 
 export async function updateOrder(id, order) {
   const url = `${baseUrl}/orders/${id}`;
-  const putHeaders = {
-    'Authorization': getAuthHeader(),
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
-  }
-  const init = { method: 'PUT', headers: putHeaders, body: JSON.stringify(order)};
+  const init = { method: 'PUT', headers: createPutHeaders(), body: JSON.stringify(order)};
   return await fetchDataFromAPI(url, init);
 }
 
 export async function deleteOrder(id) {
   const url = `${baseUrl}/orders/${id}`;
-  const putHeaders = {
-    'Authorization': getAuthHeader(),
-    'Access-Control-Allow-Origin': '*',
-  }
-  const init = { method: 'DELETE', headers: putHeaders};
+  const init = { method: 'DELETE', headers: createDeleteHeaders()};
   return await fetchDataFromAPI(url, init);
 }
 
 export async function createOrder(order) {
   const url = `${baseUrl}/orders/`;
-  const postHeaders = {
-    'Authorization': getAuthHeader(),
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  }
-  const init = { method: 'POST', headers: postHeaders, body: JSON.stringify(order)};
+  const init = { method: 'POST', headers: createPostHeaders(), body: JSON.stringify(order)};
   return await fetchDataFromAPI(url, init);
 }
